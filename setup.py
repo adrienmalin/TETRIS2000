23558bf8718cc66e2aa3a9f82c1b0a40c1682d3b
adrienmalin <41926238+adrienmalin@users.noreply.github.com>
2018-07-30 22:24:13 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Tetris2000",
+    version="0.2",
+    author="Adrien Malin",
+    author_email="adrien.malin@protonmail.com",
+    description="Another Tetris clone",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adrienmalin/Tetris2000",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
